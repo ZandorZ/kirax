0aafd4ec565038de918346b0aeb0df4d402a207d
ZandorZ <sancwb@gmail.com>
2020-07-02 20:25:06 -0300

    Using Observables

--- kirax.go
+++ kirax.go
@@ -10,6 +10,7 @@ import (
 	"github.com/mcuadros/go-lookup"
 	"github.com/modern-go/reflect2"
 	"github.com/r3labs/diff"
+	"github.com/reactivex/rxgo/v2"
 )
 
 // Action ...
@@ -18,6 +19,7 @@ type Action struct {
 	Payload interface{}
 }
 
+// SnapShot ...
 type SnapShot struct {
 	time.Time
 	Data  interface{}
@@ -37,12 +39,14 @@ type Store struct {
 	listeners       map[string]Listener
 	modifiers       map[string]Modifier
 	modifierMethods map[string]reflect.Value
+	stateCh         chan rxgo.Item
 }
 
 // NewStore ...
 func NewStore(initState interface{}) *Store {
 	return &Store{
 		state:           reflect.ValueOf(initState), // TODO: check if struct or pointer to struct
+		stateCh:         make(chan rxgo.Item),
 		listeners:       make(map[string]Listener),
 		modifiers:       make(map[string]Modifier),
 		modifierMethods: make(map[string]reflect.Value),
@@ -83,6 +87,11 @@ func (s *Store) AddModifierMethod(action string, modifier interface{}) error {
 	return nil
 }
 
+// NewObservable creates new state observable
+func (s *Store) NewObservable() rxgo.Observable {
+	return rxgo.FromChannel(s.stateCh, rxgo.WithPublishStrategy())
+}
+
 //Dispatch ...
 func (s *Store) Dispatch(action Action) error {
 	s.Lock()
@@ -95,6 +104,7 @@ func (s *Store) Dispatch(action Action) error {
 
 	if mod, ok := s.modifiers[action.Name]; ok {
 		mod(s.state.Interface(), action.Payload)
+		s.stateCh <- rxgo.Of(s.state.Interface())
 		return s.checkState(oldV)
 	}
 
@@ -124,6 +134,8 @@ func (s *Store) Dispatch2(action Action) error {
 		} else {
 			mod.Call([]reflect.Value{s.state})
 		}
+		s.stateCh <- rxgo.Of(s.state.Elem().Interface())
+		return nil //TODO checkState()
 
 	}
 	return fmt.Errorf("Action '%s' not found", action.Name)
